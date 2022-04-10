ee2dbc60b5c03b7771ac8cfa5f4772f0d24e5651
sheelu <sheelkumar558@gmail.com>
2022-04-11 00:27:19 +0530

    sk

--- day3/Inventory/src/App.jsx
+++ day3/Inventory/src/App.jsx
@@ -1,115 +1,28 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { Inventory } from "./components/Inventory";
 
-export const App = () => {
-  // const [inv, setInv] = useState({
-  //   books: 10,
-  //   notebooks: 13,
-  //   pens: 40,
-  // });
 
-  const [books, setBooks] = useState(10);
-  const [notebooks, setNotebooks] = useState(13);
-  const [pens, setPens] = useState(40);
 
-  // Create add and remove functions here that changes the
-  const [total, setTotal] = useState(books + notebooks + pens);
 
-  // state.
+function App() {
+
+  const [count,SetCount]=useState(0);
+ 
+  const counthendeller=(value)=>{
+    // if(count<0){
+    //   return;
+    // }
+    SetCount(count+value);
+  }
+
   return (
-    <>
-      <div
-        style={{
-          border: "1px solid black",
-          borderRadius: "3px",
-          padding: "10px",
-          display: "flex",
-          flexDirection: "column",
-          width: "400px",
-          gap: "20px",
-        }}
-      >
-        <div className="items">
-          <span>Books: </span>
-          <button
-            className="circlularButton"
-            onClick={() => {
-              setBooks(books + 1);
-              setTotal(total + 1);
-            }}
-          >
-            +
-          </button>
-          <button
-            className="circlularButton"
-            onClick={() => {
-              if (books <= 0) {
-                return;
-              }
-              setBooks(books - 1);
-              setTotal(total - 1);
-            }}
-          >
-            -
-          </button>
-          <span>{books}</span>
-        </div>
-        <div className="items">
-          <span>Notebooks: </span>
-          <button
-            className="circlularButton"
-            onClick={() => {
-              setNotebooks(notebooks + 1);
-              setTotal(total + 1);
-            }}
-          >
-            +
-          </button>
-          <button
-            className="circlularButton"
-            onClick={() => {
-              if (notebooks <= 0) {
-                return;
-              }
-              setNotebooks(notebooks - 1);
-              setTotal(total - 1);
-            }}
-          >
-            -
-          </button>
-          <span>{notebooks}</span>
-        </div>
-        <div className="items">
-          <span>Pen: </span>
-          <button
-            className="circlularButton"
-            onClick={() => {
-              setPens(pens + 1);
-              setTotal(total + 1);
-            }}
-          >
-            +
-          </button>
-          <button
-            className="circlularButton"
-            onClick={() => {
-              if (pens <= 0) {
-                return;
-              }
-              setPens(pens - 1);
-              setTotal(total - 1);
-            }}
-          >
-            -
-          </button>
-          <span>{pens}</span>
-        </div>
-        {/* calculate total and show it */}
-        total: {total}
-      </div>
-      <br />
-    </>
+    <div className="App">
+     
+      <Inventory />
+     
+    </div>
   );
-};
+}
 
 export default App;
